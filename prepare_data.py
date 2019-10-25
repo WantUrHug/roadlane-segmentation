b5798bfb2e5f296b5ff9f7012cb5e2137eb19143
WantUrHug <602141901@qq.com>
2019-10-25 10:57:39 +0800

    修改了data和lable存放的形式

--- prepare_data.py
+++ prepare_data.py
@@ -6,49 +6,81 @@ import os, shutil
 sparse_dir = "D:\\GitFile\\FuHua\\新建文件夹"
 dst_dir = "D:\\GitFile\\roadlane-segmentation\\imgs"
 
-data_dir = os.path.join(dst_dir, "data")
-if not os.path.exists(data_dir):
-	os.mkdir(data_dir)
-data_train_dir = os.path.join(data_dir, "train")
-if not os.path.exists(data_train_dir):
-	os.mkdir(data_train_dir)
-data_test_dir = os.path.join(data_dir, "test")
-if not os.path.exists(data_test_dir):
-	os.mkdir(data_test_dir)
+#原本是先创建 data 和 label 文件夹，然后在他们之下再分别都创建
+#train 和 test，后来发现这样子做不方便，因为在输入时需要成对给入网络
+#如果 train_data 和 train_label 是在两个不同的路径下，不方便
 
-label_dir = os.path.join(dst_dir, "label")
-if not os.path.exists(label_dir):
-	os.mkdir(label_dir)
-label_train_dir = os.path.join(label_dir, "train")
-if not os.path.exists(label_train_dir):
-	os.mkdir(label_train_dir)
-label_test_dir = os.path.join(label_dir, "test")
-if not os.path.exists(label_test_dir):
-	os.mkdir(label_test_dir)
+#data_dir = os.path.join(dst_dir, "data")
+#if not os.path.exists(data_dir):
+#	os.mkdir(data_dir)
+#data_train_dir = os.path.join(data_dir, "train")
+#if not os.path.exists(data_train_dir):
+#	os.mkdir(data_train_dir)
+#data_test_dir = os.path.join(data_dir, "test")
+#if not os.path.exists(data_test_dir):
+#	os.mkdir(data_test_dir)
+#
+#label_dir = os.path.join(dst_dir, "label")
+#if not os.path.exists(label_dir):
+#	os.mkdir(label_dir)
+#label_train_dir = os.path.join(label_dir, "train")
+#if not os.path.exists(label_train_dir):
+#	os.mkdir(label_train_dir)
+#label_test_dir = os.path.join(label_dir, "test")
+#if not os.path.exists(label_test_dir):
+#	os.mkdir(label_test_dir)
+
+train_dir = os.path.join(dst_dir, "train")
+if not os.path.exists(train_dir):
+	os.mkdir(train_dir)
+train_data_dir = os.path.join(train_dir, "data")
+train_label_dir = os.path.join(train_dir, "label")
+if not os.path.exists(train_data_dir):
+	os.mkdir(train_data_dir)
+if not os.path.exists(train_label_dir):
+	os.mkdir(train_label_dir)
+
+test_dir = os.path.join(dst_dir, "test")
+if not os.path.exists(test_dir):
+	os.mkdir(test_dir)
+test_data_dir = os.path.join(test_dir, "data")
+test_label_dir = os.path.join(test_dir, "label")
+if not os.path.exists(test_data_dir):
+	os.mkdir(test_data_dir)
+if not os.path.exists(test_label_dir):
+	os.mkdir(test_label_dir)
 
 
-train_size = 79
-test_size = 20
-i = 0
+train_size = 0.8
+total = 0
+item_namels = []
 for item in os.listdir(sparse_dir):
 	if item.endswith("_json"):
-		#print(item)
+		total += 1
 		item_name = item[:-5]
-		#print(item_name)
-		if i <= 79:
-			json_path = os.path.join(sparse_dir, item)
-			img_path = os.path.join(json_path, "img.png")
-			img_dst = os.path.join(data_train_dir, item_name + ".png")
-			label_path = os.path.join(json_path, "label.png")
-			label_dst = os.path.join(label_train_dir, item_name + ".png")
-			shutil.copyfile(img_path, img_dst)
-			shutil.copyfile(label_path, label_dst)
-			i += 1
-		else:
-			json_path = os.path.join(sparse_dir, item)
-			img_path = os.path.join(json_path, "img.png")
-			img_dst = os.path.join(data_test_dir, item_name + ".png")
-			label_path = os.path.join(json_path, "label.png")
-			label_dst = os.path.join(label_test_dir, item_name + ".png")
-			shutil.copyfile(img_path, img_dst)
-			shutil.copyfile(label_path, label_dst)
+		item_namels.append(item_name)
+print("You have prepare %d pictures and their labels.")
+
+train_num = int(total * train_size)
+test_num = total - train_num
+print("Train number: %d, test number: %d."%(train_num, test_num))
+
+cnt = 1
+for item_name in item_namels:
+	if cnt <= train_num:
+		json_path = os.path.join(sparse_dir, item_name + "_json")
+		img_path = os.path.join(json_path, "img.png")
+		img_dst = os.path.join(train_data_dir, item_name + ".png")
+		label_path = os.path.join(json_path, "label.png")
+		label_dst = os.path.join(train_label_dir, item_name + ".png")
+		shutil.copyfile(img_path, img_dst)
+		shutil.copyfile(label_path, label_dst)
+		cnt += 1
+	else:
+		json_path = os.path.join(sparse_dir, item_name + "_json")
+		img_path = os.path.join(json_path, "img.png")
+		img_dst = os.path.join(test_data_dir, item_name + ".png")
+		label_path = os.path.join(json_path, "label.png")
+		label_dst = os.path.join(test_label_dir, item_name + ".png")
+		shutil.copyfile(img_path, img_dst)
+		shutil.copyfile(label_path, label_dst)
