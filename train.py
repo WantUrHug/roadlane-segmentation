28e65fe0443373242a60b6dad788c7680eb3c00c
陈百见 <>
2019-12-13 17:38:09 +0800

    修改图片的输入为-1到1，以及相应调整

--- train.py
+++ train.py
@@ -8,16 +8,16 @@ import utils
 
 os.environ['CUDA_VISIBLE_DEVICES']='0'
 
-BATCH_SIZE = 4
+BATCH_SIZE = 8
 #不使用原尺寸，选择 RANDOM_CROP
-IMG_W = 1920
-IMG_H = 1056
+IMG_W = 640
+IMG_H = 1280
 IMG_C = 3
-NUM_CLASSES = 2
+NUM_CLASSES = 1
 
-MAXSTEP = 1000
-CHECK_STEP = 5
-SAVE_STEP = 200
+MAXSTEP = 2000
+CHECK_STEP = 10
+SAVE_STEP = 400
 
 train_dir = "D:\\GitFile\\roadlane-segmentation-imgs\\train"
 test_dir = "D:\\GitFile\\roadlane-segmentation-imgs\\test"
@@ -39,32 +39,33 @@ print("*******************************************************")
 print("* Finish step 1: preparing data. *")
 
 
-X = tf.placeholder(tf.float32, [BATCH_SIZE, IMG_H, IMG_W, IMG_C], name = "X")
-Y = tf.placeholder(tf.float32, [BATCH_SIZE, IMG_H, IMG_W, NUM_CLASSES])
+X = tf.placeholder(tf.float32, [None, IMG_H, IMG_W, IMG_C], name = "X")
+Y = tf.placeholder(tf.float32, [None, IMG_H, IMG_W, NUM_CLASSES])
 _pred = GCN.build_gcn(X, NUM_CLASSES)
 tf.add_to_collection("infrence", _pred)
 
 #logits 是前向传播的结果，labels 是正确的标签
 #print(_pred.shape, Y.shape)
-_pred_flatten = utils.label_before_cal_loss(_pred)
-Y_flatten = utils.label_before_cal_loss(Y)
+_pred_flatten = _pred#utils.label_before_cal_loss(_pred)
+Y_flatten = Y#utils.label_before_cal_loss(Y)
 #_pred_flatten = tf.concat()
 #print(_pred_flatten[0].shape)
 #print("len of prepare: ", utils.label_before_cal_loss(_pred).shape) #tf.layers.Flatten()(_pred)
 #Y_flatten = tf.layers.Flatten()(Y)
-print(_pred_flatten.shape)
-print(Y_flatten.shape)
+#print(_pred_flatten.shape)
+#print(Y_flatten.shape)
 #softmax = tf.nn.softmax_cross_entropy_with_logits(logits = _pred_flatten, labels = Y_flatten)
-loss = utils.weighted_loss(logits = _pred_flatten, labels = Y_flatten)#tf.reduce_mean(softmax)
-acc = utils.accuracy(logits = _pred_flatten, labels = Y_flatten)
-print(loss.shape)
-print("1")
+loss = utils.weighted_loss_v1(logits = _pred_flatten, labels = Y_flatten)#tf.reduce_mean(softmax)
+acc = utils.total_accuracy(logits = _pred_flatten, labels = Y_flatten)
+#print(loss.shape)
+
+#print("1")
 global_step = tf.Variable(0, trainable = False)
-print("2")
+#print("2")
 opt = tf.train.AdamOptimizer(learning_rate = 0.0001).minimize(loss, global_step = global_step)
-print("3")
+#print("3")
 #global_acc = utils.cal_global_accuracy(_pred, Y)
-print("4")
+#print("4")
 #history = {}
 #history["train_loss"] = []
 #history["test_loss"] = []
@@ -89,15 +90,15 @@ with tf.Session(config = config) as sess:
 
 		train_data, train_label = sess.run((next_train_data, next_train_label))
 		with tf.device("/cpu:0"):
-			train_data = np.float32(train_data)/255.0
+			train_data = (np.float32(train_data)/255.0-0.5)*2
 		
 		if step % CHECK_STEP == 0:
 			test_data, test_label = sess.run((next_test_data, next_test_label))
 			with tf.device("/cpu:0"):
-				test_data = np.float32(test_data)/255.0
+				test_data = (np.float32(test_data)/255.0-0.5)*2
 			_, train_loss_value, train_acc_value = sess.run((opt, loss, acc), feed_dict = {X: train_data, Y: train_label})
 			_, test_loss_value, test_acc_value = sess.run((opt, loss, acc), feed_dict = {X: test_data, Y: test_label})
-
+			#print(train_acc_value, train_loss_value)
 			#history["train_loss"].append(train_loss_value)
 			#history["train_acc"].append(train_acc_value)
 			#history["test_loss"].append(test_loss_value)
@@ -111,7 +112,5 @@ with tf.Session(config = config) as sess:
 
 		else:
 			sess.run(opt, feed_dict = {X: train_data, Y: train_label})
-
 		if step%SAVE_STEP == 0:
 			saver.save(sess, os.path.join(model_dir, "gcn"), global_step = step)
-
